05afbfd5f6b7f77da44ea639ea694e830867bb16
Nguyen Nam Son <namson98@gmail.com>
2020-11-02 14:59:00 +0100

    simplify script + new corrplot

--- main_script.R
+++ main_script.R
@@ -16,17 +16,15 @@ library(summarytools)
 library(stargazer)
 library(ggplot2)
 library(reshape2)
-library(patchwork)
 library(Hmisc)
+library(corrplot)
 
 wd <- file.path('~', 'microecon_final')
 setwd(wd)
 
 df <- read_dta("Bonjour_data.dta")
 
-glimpse(df)
-
-#pooled
+# Descriptive stats -------------------------------------------------------
 
 dfsum <- df[, c('bweight', 'earning', 'schyear', 'highqua', 'age', 'married', 'full', 'own_exp', 'sm16', 'sm18')] %>%
   dfSummary(., plain.ascii = FALSE, style = "grid", 
@@ -34,6 +32,96 @@ dfsum <- df[, c('bweight', 'earning', 'schyear', 'highqua', 'age', 'married', 'f
 
 dfsum$Missing <- NULL
 view(dfsum, file = "~/microecon_final/dfsum.html")
+
+# Feature Selection -----------------------------------------------------------------
+
+pdf$dhighqua <- diff(pdf$highqua)
+pdf$dmarried <- diff(pdf$married)
+pdf$dbweight <- diff(pdf$bweight)
+pdf$dexp_par <- diff(pdf$exp_par)
+pdf$down_exp <- diff(pdf$own_exp)
+pdf$dfull <- diff(pdf$full)
+pdf$dpart <- diff(pdf$part)
+pdf$dself <- diff(pdf$self)
+pdf$dsm16 <- diff(pdf$sm16)
+pdf$dsm18 <- diff(pdf$sm18)
+
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+#basic features
+
+cormatdf <- df[, c('highqua',
+                   'bweight',
+                   'full',
+                   'part',
+                   'self',
+                   'married',
+                   'own_exp',
+                   'exp_par')]
+
+res1 <- rcorr(as.matrix(cormatdf))
+res11 <- flattenCorrMatrix(res1$r, res1$P)
+
+write.table(subset(res11, row == 'highqua'), file = "~/microecon_final/res1.txt", sep="\t")
+
+# Insignificant correlation are crossed
+corrplot(res1$r, type="upper", order="hclust", p.mat = res1$P, sig.level = 0.05, tl.col = "black", tl.srt = 45)
+
+#differenced features
+
+cormatdf <- pdf[, c('dhighqua',
+                    'dbweight',
+                    'dfull',
+                    'dpart',
+                    'dself',
+                    'dmarried',
+                    'down_exp',
+                    'dexp_par')]
+
+res2 <- rcorr(as.matrix(cormatdf))
+res22 <- flattenCorrMatrix(res2$r, res2$P)
+
+write.table(subset(res22, row == 'dhighqua'), file = "~/microecon_final/res2.txt", sep="\t")
+
+# Insignificant correlation are crossed
+corrplot(res2$r, type="upper", order="hclust", p.mat = res2$P, sig.level = 0.05, tl.col = "black", tl.srt = 45)
+
+#smoking as IV?
+
+cormatdf <- pdf[, c('highqua',
+                    'sm16',
+                    'sm18')]
+
+res3 <- rcorr(as.matrix(cormatdf))
+res33 <- flattenCorrMatrix(res3$r, res3$P)
+
+write.table(subset(res33, row == 'highqua'), file = "~/microecon_final/res3.txt", sep="\t")
+
+# Insignificant correlation are crossed
+p3 <- corrplot(res3$r, type="upper", order="hclust", 
+               p.mat = res3$P, sig.level = 0.05, tl.col = "black", tl.srt = 45)
+
+#differencing smoking
+cormatdf <- pdf[, c('dhighqua',
+                    'dsm16',
+                    'dsm18')]
+
+res4 <- rcorr(as.matrix(cormatdf))
+res44 <- flattenCorrMatrix(res4$r, res4$P)
+
+write.table(subset(res44, row == 'dhighqua'), file = "~/microecon_final/res4.txt", sep="\t")
+
+# Insignificant correlation are crossed
+p4 <- corrplot(res4$r, type="upper", order="hclust", 
+               p.mat = res4$P, sig.level = 0.05, tl.col = "black", tl.srt = 45)
 
 # Methods -----------------------------------------------------------------
 
@@ -62,260 +150,11 @@ fdr_iv2 <- plm(log(earning) ~ highqua + age + I(age**2/100) + LNandSE + married 
 #comparing all the models together
 
 stargazer(ols1, iv1, ols2, iv2, fdr_ols1, fdr_iv1, fdr_ols2, fdr_iv2, font.size = "small",
-          align = TRUE, type = 'text',  out="models1.htm")
+          align = TRUE, type = 'text',  out = "models1.htm")
 
 #smoking as IV
 ivsm16 <- ivreg(log(earning) ~ highqua + age + I(age**2/100) | age + I(age**2/100) + sm16,  data=df)
 ivsm18 <- ivreg(log(earning) ~ highqua + age + I(age**2/100) | age + I(age**2/100) + sm18,  data=df)
 
 stargazer(ols1, ivsm16, ivsm18, type = 'text', out = "models2.htm")
-
-# Correlations -----------------------------------------------------------------
-
-pdf$dhighqua <- diff(pdf$highqua)
-pdf$dmarried <- diff(pdf$married)
-pdf$dbweight <- diff(pdf$bweight)
-pdf$dexp_par <- diff(pdf$exp_par)
-pdf$down_exp <- diff(pdf$own_exp)
-pdf$dfull <- diff(pdf$full)
-pdf$dpart <- diff(pdf$part)
-pdf$dself <- diff(pdf$self)
-pdf$dsm16 <- diff(pdf$sm16)
-pdf$dsm18 <- diff(pdf$sm18)
-
-cormatdf <- df[, c('highqua',
-                   'bweight',
-                   'full',
-                   'part',
-                   'self',
-                   'married',
-                   'own_exp',
-                   'exp_par')]
-
-cormat <- cormatdf %>% cor(use = "complete.obs") %>% round(., 2)
-
-flattenCorrMatrix <- function(cormat, pmat) {
-  ut <- upper.tri(cormat)
-  data.frame(
-    row = rownames(cormat)[row(cormat)[ut]],
-    column = rownames(cormat)[col(cormat)[ut]],
-    cor  =(cormat)[ut],
-    p = pmat[ut]
-  )
-}
-
-res1 <- rcorr(as.matrix(cormatdf), type = 'pearson')
-res1 <- flattenCorrMatrix(res1$r, res1$P)
-
-write.table(subset(res1, row == 'highqua'), file = "~/microecon_final/res1.txt", sep="\t")
-
-# Get lower triangle of the correlation matrix
-get_lower_tri<-function(cormat){
-  cormat[lower.tri(cormat)] <- NA
-  return(cormat)
-}
-
-cormat <- reorder_cormat(cormat)
-lower_tri <- get_lower_tri(cormat)
-
-# Melt the correlation matrix
-melted_cormat <- melt(lower_tri, na.rm = TRUE)
-
-# Create a ggheatmap
-
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value)) +
-  geom_tile(color = "white") +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1) )+
-  coord_fixed()
-
-p1 <- ggheatmap + 
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                               title.position = "top", title.hjust = 0.5)) +
-  labs(title = "Correlation heatmap between non-differenced variables",
-       subtitle = "for feature selection of control variables")
-
-
-cormatdf <- pdf[, c('dhighqua',
-                    'dbweight',
-                    'dfull',
-                    'dpart',
-                    'dself',
-                    'dmarried',
-                    'down_exp',
-                    'dexp_par')]
-
-res2 <- rcorr(as.matrix(cormatdf))
-res2 <- flattenCorrMatrix(res2$r, res2$P)
-
-write.table(subset(res2, row == 'dhighqua'), file = "~/microecon_final/res2.txt", sep="\t")
-
-#viz
-
-cormat <- cormatdf %>% cor(use = "complete.obs") %>% round(., 2)
-
-# Get lower triangle of the correlation matrix
-get_lower_tri<-function(cormat){
-  cormat[lower.tri(cormat)] <- NA
-  return(cormat)
-}
-
-cormat <- reorder_cormat(cormat)
-lower_tri <- get_lower_tri(cormat)
-
-# Melt the correlation matrix
-melted_cormat <- melt(lower_tri, na.rm = TRUE)
-
-# Create a ggheatmap
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value)) +
-  geom_tile(color = "white") +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1) )+
-  coord_fixed()
-
-p2 <- ggheatmap + 
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                               title.position = "top", title.hjust = 0.5)) +
-  labs(title = "Correlation heatmap of differenced variables",
-       subtitle = "differencing reduced the correlation between regressors")
-
-
-p1 + p2
-
-#smoking as IV?
-
-cormatdf <- pdf[, c('highqua',
-                    'sm16',
-                    'sm18')]
-
-res3 <- rcorr(as.matrix(cormatdf))
-res3 <- flattenCorrMatrix(res3$r, res3$P)
-
-write.table(subset(res3, row == 'highqua'), file = "~/microecon_final/res3.txt", sep="\t")
-
-cormat3 <- cormatdf %>% cor(use = "complete.obs") %>% round(., 2)
-
-# Get lower triangle of the correlation matrix
-get_lower_tri<-function(cormat){
-  cormat[lower.tri(cormat)] <- NA
-  return(cormat)
-}
-
-cormat <- reorder_cormat(cormat)
-lower_tri <- get_lower_tri(cormat)
-
-# Melt the correlation matrix
-melted_cormat <- melt(lower_tri, na.rm = TRUE)
-
-# Create a ggheatmap
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value)) +
-  geom_tile(color = "white") +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1) )+
-  coord_fixed()
-
-p3 <- ggheatmap + 
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                               title.position = "top", title.hjust = 0.5)) +
-  labs(title = "Correlation heatmap of differenced variables",
-       subtitle = "differencing reduced the correlation between education and smoking")
-
-#differencing smoking
-cormatdf <- pdf[, c('dhighqua',
-                    'dsm16',
-                    'dsm18')]
-
-res4 <- rcorr(as.matrix(cormatdf))
-res4 <- flattenCorrMatrix(res4$r, res4$P)
-
-write.table(subset(res4, row == 'dhighqua'), file = "~/microecon_final/res4.txt", sep="\t")
-
-#viz
-cormat4 <- cormatdf %>% cor(use = "complete.obs") %>% round(., 2)
-# Get lower triangle of the correlation matrix
-get_lower_tri<-function(cormat){
-  cormat[lower.tri(cormat)] <- NA
-  return(cormat)
-}
-
-cormat <- reorder_cormat(cormat)
-lower_tri <- get_lower_tri(cormat)
-
-# Melt the correlation matrix
-melted_cormat <- melt(lower_tri, na.rm = TRUE)
-
-# Create a ggheatmap
-ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value)) +
-  geom_tile(color = "white") +
-  scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1) )+
-  coord_fixed()
-
-p4 <- ggheatmap + 
-  geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
-  theme(
-    axis.title.x = element_blank(),
-    axis.title.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.border = element_blank(),
-    panel.background = element_blank(),
-    axis.ticks = element_blank(),
-    legend.justification = c(1, 0),
-    legend.position = c(0.6, 0.7),
-    legend.direction = "horizontal")+
-  guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
-                               title.position = "top", title.hjust = 0.5)) +
-  labs(title = "Correlation heatmap of differenced variables",
-       subtitle = "differencing reduced the correlation between education and smoking")
-
-p3 + p4
 
